1761fda0139f74c8cdaad7d524f9eb99bded7943
ajrat <ayrat.mub@gmail.com>
2017-09-17 19:01:33 +0300

    Заливаем CMS попытка 2

--- admin/index.php
+++ admin/index.php
@@ -1,189 +1,42 @@
 <?php
-
 /**
- * Monstra
+ * Login
  *
- * @package Monstra
- * @author Romanenko Sergey / Awilum <awilum@msn.com>
- * @link http://monstra.org
+ * Allows access to the GetSimple control panel
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @package GetSimple
+ * @subpackage Login
  */
 
+# Setup inclusions
+$load['login'] = true;
+$load['plugin'] = true;
 
-// Main engine defines
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', rtrim(str_replace(array('admin'), array(''), dirname(__FILE__)), '\\/'));
-define('BACKEND', true);
-define('MONSTRA_ACCESS', true);
-
-// Load Monstra engine _init.php file
-require_once ROOT. DS .'engine'. DS .'_init.php';
-
-// Errors var when users login failed
-$login_error = '';
-
-// Get users Table
-$users = new Table('users');
-
-// Admin login
-if (Request::post('login_submit')) {
-    if (Cookie::get('login_attempts') && Cookie::get('login_attempts') >= 5) {
-        $login_error = __('You are banned for 10 minutes. Try again later', 'users');
-    } else {
-        $user = $users->select("[login='" . trim(Request::post('login')) . "']", null);
-        if (count($user) !== 0) {
-            if ($user['login'] == Request::post('login')) {
-                if (trim($user['password']) == Security::encryptPassword(Request::post('password'))) {
-                    if ($user['role'] == 'admin' || $user['role'] == 'editor') {
-                        Session::set('admin', true);
-                        Session::set('user_id', (int) $user['id']);
-                        Session::set('user_login', (string) $user['login']);
-                        Session::set('user_role', (string) $user['role']);
-                        Session::set('user_email', (string) $user['email']);
-                        Request::redirect('index.php');
-                    }
-                } else {
-                    $login_error = __('Wrong <b>username</b> or <b>password</b>', 'users');
-
-                    if (Cookie::get('login_attempts')) {
-                        if (Cookie::get('login_attempts') < 5) {
-                            $attempts = Cookie::get('login_attempts') + 1;
-                            Cookie::set('login_attempts', $attempts, 600);
-                        } else {
-                            $login_error = __('You are banned for 10 minutes. Try again later', 'users');
-                        }
-                    } else {
-                        Cookie::set('login_attempts', 1, 600);
-                    }
-                }
-            }
-        } else {
-            $login_error = __('Wrong <b>username</b> or <b>password</b>', 'users');
-
-            if (Cookie::get('login_attempts')) {
-                if (Cookie::get('login_attempts') < 5) {
-                    $attempts = Cookie::get('login_attempts') + 1;
-                    Cookie::set('login_attempts', $attempts, 600);
-                } else {
-                    $login_error = __('You are banned for 10 minutes. Try again later', 'users');
-                }
-            } else {
-                Cookie::set('login_attempts', 1, 600);
-            }
-        }
-    }
-
-    Notification::setNow('error', $login_error);
-}
-
-// Errors
-$errors = array();
-
-$site_url  = Option::get('siteurl');
-$site_name = Option::get('sitename');
-
-$user_login = trim(Request::post('login'));
-
-// Reset Password Form Submit
-if (Request::post('reset_password_submit')) {
-    if (Option::get('captcha_installed') == 'true' && ! CryptCaptcha::check(Request::post('answer'))) {
-        $errors['users_captcha_wrong'] = __('Captcha code is wrong', 'users');
-    }
-    if ($user_login == '') {
-        $errors['users_empty_field'] = __('Required field', 'users');
-    }
-    if ($user_login != '' && ! $users->select("[login='".$user_login."']")) {
-        $errors['users_user_doesnt_exists'] = __('This user doesnt exist', 'users');
-    }
-
-    if (count($errors) == 0) {
-
-        // Get user
-        $user = $users->select("[login='" . $user_login . "']", null);
-
-        // Generate new hash
-        $new_hash = Text::random('alnum', 12);
-
-        // Update user hash
-        $users->updateWhere("[login='" . $user_login . "']", array('hash' => $new_hash));
-
-        $mail = new PHPMailer();
-        $mail->CharSet = 'utf-8';
-        $mail->ContentType = 'text/html';
-        $mail->SetFrom(Option::get('system_email'));
-        $mail->AddReplyTo(Option::get('system_email'));
-        $mail->AddAddress($user['email'], $user['login']);
-        $mail->Subject = __('Your login details for :site_name', 'users', array(':site_name' => $site_name));
-        $mail->MsgHTML(View::factory('box/emails/views/emails/email_layout')
-            ->assign('site_url', $site_url)
-            ->assign('site_name', $site_name)
-            ->assign('user_id', $user['id'])
-            ->assign('user_login', $user['login'])
-            ->assign('new_hash', $new_hash)
-            ->assign('email_template', 'reset-password')
-            ->render());
-        $mail->Send();
-
-        // Set notification
-        Notification::set('success', __('Your login details for :site_name has been sent', 'users', array(':site_name' => $site_name)));
-        Notification::set('reset_password', 'reset_password');
-
-        // Redirect to password-reset page
-        Request::redirect(Site::url().'/admin');
-    }
-
-    Notification::setNow('reset_password', 'reset_password');
-}
-
-// If admin user is login = true then set is_admin = true
-if (Session::exists('admin') && Session::get('admin') == true) {
-    $is_admin = true;
-} else {
-    $is_admin = false;
-}
-
-// Logout user from system
-if (Request::get('logout') && Request::get('logout') == 'do') {
-    Session::destroy();
-}
-
-// If is admin then load admin area
-if ($is_admin) {
-
-    // If id is empty then redirect to default plugin PAGES
-    if (Request::get('id')) {
-        $area = Request::get('id');
-    } else {
-        Request::redirect(Site::url().'/admin/index.php?id=dashboard');
-    }
-
-    $plugins_registered = Plugin::$plugins;
-    foreach ($plugins_registered as $plugin) {
-        $plugins_registered_areas[] = $plugin['id'];
-    }
-
-    // Show plugins admin area only for registered plugins
-    if (in_array($area, $plugins_registered_areas)) {
-        $plugin_admin_area = true;
-    } else {
-        $plugin_admin_area = false;
-    }
-
-    // Backend pre render
-    Action::run('admin_pre_render');
-
-    // Display admin template
-    require 'themes'. DS . Option::get('theme_admin_name') . DS . 'index.template.php';
-
-    // Backend post render
-    Action::run('admin_post_render');
-} else {
-
-    // Display login template
-    require 'themes'. DS . Option::get('theme_admin_name') . DS . 'login.template.php';
-}
-
-// Flush (send) the output buffer and turn off output buffering
+// wrap all include and header output in output buffering to prevent sending before headers.
+ob_start();
+	include('inc/common.php');
+	get_template('header', cl($SITENAME).' &raquo; '.i18n_r('LOGIN')); 
 ob_end_flush();
+
+?>
+
+</div>
+</div>
+<div class="wrapper">
+<?php include('template/error_checking.php'); ?>
+<div class="bodycontent clearfix">
+	<div id="maincontent">
+		<div class="main" >
+			<h3><?php echo cl($SITENAME); ?></h3>
+			<?php exec_action('index-login'); ?>
+			<form class="login" action="<?php echo myself(false).'?'. htmlentities($_SERVER['QUERY_STRING'], ENT_QUOTES); ?>" method="post">
+				<p><b><?php i18n('USERNAME'); ?>:</b><br /><input type="text" class="text" id="userid" name="userid" /></p>
+				<p><b><?php i18n('PASSWORD'); ?>:</b><br /><input type="password" class="text" id="pwd" name="pwd" /></p>
+				<p><input type="submit" name="submitted" class="submit" value="<?php i18n('LOGIN'); ?>" /></p>
+			</form>
+			<p class="cta" ><b>&laquo;</b> <a href="<?php echo $SITEURL; ?>"><?php i18n('BACK_TO_WEBSITE'); ?></a> &nbsp; | &nbsp; <a href="resetpassword.php"><?php i18n('FORGOT_PWD'); ?></a> &raquo;</p>
+			<div class="reqs" ><?php exec_action('login-reqs'); ?></div>
+		</div>
+	</div>
+</div>
+<?php get_template('footer'); ?>
--- index.php
+++ index.php
@@ -1,63 +1,139 @@
 <?php
-
 /**
- * Monstra
+ * Index
  *
- * @package Monstra
- * @author Romanenko Sergey / Awilum <awilum@msn.com>
- * @link http://monstra.org
+ * Where it all starts	
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @package GetSimple
+ * @subpackage FrontEnd
  */
 
 
-// Main engine defines
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', rtrim(dirname(__FILE__), '\\/'));
-define('BACKEND', false);
-define('MONSTRA_ACCESS', true);
+/* pre-common setup, load gsconfig and get GSADMIN path */
 
-// First check for installer then go
-if (file_exists('install.php')) {
-    if (isset($_GET['install'])) {
-        if ($_GET['install'] == 'done') {
-            // Try to delete install file if not delete manually
-            @unlink('install.php');
-            // Redirect to main page
-            header('location: index.php');
-        }
-    } else {
-        include 'install.php';
-    }
+	/* GSCONFIG definitions */
+	if(!defined('GSFRONT')) define('GSFRONT',1);
+	if(!defined('GSBACK'))  define('GSBACK',2);
+	if(!defined('GSBOTH'))  define('GSBOTH',3);	
+	if(!defined('GSSTYLEWIDE')) define('GSSTYLEWIDE','wide'); // wide style sheet
+	if(!defined('GSSTYLE_SBFIXED')) define('GSSTYLE_SBFIXED','sbfixed'); // fixed sidebar
+
+	# Check and load gsconfig
+	if (file_exists('gsconfig.php')) {
+		require_once('gsconfig.php');
+	}
+
+	# Apply GSADMIN env
+	if (defined('GSADMIN')) {
+		$GSADMIN = GSADMIN;
+	} else {
+		$GSADMIN = 'admin';
+	}
+
+	# setup paths 
+	# @todo wtf are these for ?
+	$admin_relative = $GSADMIN.'/inc/';
+	$lang_relative = $GSADMIN.'/';
+
+	$load['plugin'] = true;
+	$base = true;
+
+/* end */
+
+# Include common.php
+include($GSADMIN.'/inc/common.php');
+
+# Hook to load page Cache
+exec_action('index-header');
+
+# get page id (url slug) that is being passed via .htaccess mod_rewrite
+if (isset($_GET['id'])){ 
+	$id = str_replace ('..','',$_GET['id']);
+	$id = str_replace ('/','',$id);
+	$id = lowercase($id);
 } else {
-
-    // Load Engine init file
-    require_once ROOT. DS . 'engine'. DS . '_init.php';
-
-    // Check for maintenance mod
-    if ('on' == Option::get('maintenance_status')) {
-
-        // Set maintenance mode for all except admin and editor
-        if ((Session::exists('user_role')) and (Session::get('user_role') == 'admin' or Session::get('user_role') == 'editor')) {
-            // Monstra show this page :)
-        } else {
-            header('HTTP/1.1 503 Service Temporarily Unavailable');
-            header('Status: 503 Service Temporarily Unavailable');
-            header('Retry-After: 600');
-            die(Text::toHtml(Option::get('maintenance_message')));
-        }
-    }
-
-    // Frontend pre render
-    Action::run('frontend_pre_render');
-
-    // Load site template
-    require MINIFY . DS . 'theme.' . Site::theme() . '.' . Site::template() . '.template.php';
-
-    // Frontend pre render
-    Action::run('frontend_post_render');
-
-    // Flush (send) the output buffer and turn off output buffering
-    ob_end_flush();
+	$id = "index";
 }
+
+// filter to modify page id request
+$id = exec_filter('indexid',$id);
+ // $_GET['id'] = $id; // support for plugins that are checking get?
+
+# define page, spit out 404 if it doesn't exist
+$file_404 = GSDATAOTHERPATH . '404.xml';
+$user_created_404 = GSDATAPAGESPATH . '404.xml';
+$data_index = null;
+
+// apply page data if page id exists
+if (isset($pagesArray[$id])) {
+	$data_index = getXml(GSDATAPAGESPATH . $id . '.xml');
+} 
+
+// filter to modify data_index obj
+$data_index = exec_filter('data_index',$data_index);
+
+// page not found handling
+if(!$data_index) {	
+	if (isset($pagesArray['404'])) {
+		// use user created 404 page
+		$data_index = getXml($user_created_404);		
+	} elseif (file_exists($file_404))	{
+		// default 404
+		$data_index = getXml($file_404);
+	} else {
+		// fail over
+		redirect('404');
+	} 	
+	exec_action('error-404');
+}
+
+$title         = $data_index->title;
+$date          = $data_index->pubDate;
+$metak         = $data_index->meta;
+$metad         = $data_index->metad;
+$url           = $data_index->url;
+$content       = $data_index->content;
+$parent        = $data_index->parent;
+$template_file = $data_index->template;
+$private       = $data_index->private;	
+
+// after fields from dataindex, can modify globals here or do whatever by checking them
+exec_action('index-post-dataindex');
+
+# if page is private, check user
+if ($private == 'Y') {
+	if (isset($USR) && $USR == get_cookie('GS_ADMIN_USERNAME')) {
+		//ok, allow the person to see it then
+	} else {
+		redirect('404');
+	}
+}
+
+# if page does not exist, throw 404 error
+if ($url == '404') {
+	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+}
+
+# check for correctly formed url
+if (getDef('GSCANONICAL',true)) {
+	if ($_SERVER['REQUEST_URI'] != find_url($url, $parent, 'relative')) {
+		redirect(find_url($url, $parent));
+	}
+}
+
+# include the functions.php page if it exists within the theme
+if ( file_exists(GSTHEMESPATH .$TEMPLATE."/functions.php") ) {
+	include(GSTHEMESPATH .$TEMPLATE."/functions.php");	
+}
+
+# call pretemplate Hook
+exec_action('index-pretemplate');
+
+# include the template and template file set within theme.php and each page
+if ( (!file_exists(GSTHEMESPATH .$TEMPLATE."/".$template_file)) || ($template_file == '') ) { $template_file = "template.php"; }
+include(GSTHEMESPATH .$TEMPLATE."/".$template_file);
+
+# call posttemplate Hook
+exec_action('index-posttemplate');
+
+?>
